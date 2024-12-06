5dea4cfc1f05ae1a405c712cfdcd1d0eec1104b7
MihajloZivkovic1 <85947846+MihajloZivkovic1@users.noreply.github.com>
2024-12-06 13:04:32 +0100

    ui

--- components/profileStories.tsx
+++ components/profileStories.tsx
@@ -6,6 +6,12 @@ import { MdEditor } from 'md-editor-rt';
 import 'md-editor-rt/lib/style.css';
 import 'md-editor-rt/lib/preview.css';
 import { Input } from './ui/input';
+import { Collapsible, CollapsibleContent } from './ui/collapsible';
+import { CollapsibleTrigger } from '@radix-ui/react-collapsible';
+import { DropdownMenuLabel } from './ui/dropdown-menu';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+import { Trash2 } from 'lucide-react';
+
 
 interface Story {
   id: string;
@@ -17,7 +23,6 @@ export default function ProfileStories({ id }: { id: string | undefined }) {
   const [stories, setStories] = useState<Story[]>([]);
   const [text, setText] = useState('');
   const [title, setTitle] = useState('');
-  // const [successMessage, setSuccessMessage] = useState("");
   const [storyMessages, setStoryMessages] = useState<Record<string, string>>({}); // Map of story ID to message
 
   useEffect(() => {
@@ -42,8 +47,8 @@ export default function ProfileStories({ id }: { id: string | undefined }) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           profileId: id,
-          title: 'Title',
-          text: "Text"
+          title: 'Naslov',
+          text: "Tekst"
         })
       });
 
@@ -123,43 +128,87 @@ export default function ProfileStories({ id }: { id: string | undefined }) {
 
   return (
     <div>
-      <div className='flex justify-evenly'>
-        <h1 className="text-2xl font-bold">Stories</h1>
-        <Button onClick={addNewStory} className='ml-2'>Dodaj novu pricu</Button>
-      </div>
+      <Collapsible className="grid">
+        <CollapsibleTrigger asChild>
+          <Button className="w-full justify-start text-left font-semibold">
+            Kako da kreiram zanimljive priče pokojnika?
+          </Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent asChild>
+          <p className="text-sm leading-loose text-gray-500 md:text-base dark:text-gray-400">
+            Na stranici za uređivanje priče pokojnika, dostupno je polje za unos naslova priče i teksta:
+            <br />
+            <strong>1. Dugme za dodavanje nove Priče:</strong> Kliknite dugme <em>Dodaj Priču</em> da bi ste dodali novi prozor koji predstavlja jednu priču
+            <br />
+            <strong>2. U delu za naslov dodajte naslov priče:</strong>
+            <br />
+            <strong>3. U delu za tekst priče napisite kratak tekst o nekoj zanimljivoj priči pokojnika</strong>
+            <br />
+            <strong>4. Dugme za čuvanje nove Priče:</strong> Kliknite dugme <em>Sačuvaj</em> da bi ste sačuvali.
+            <br />
+          </p>
+        </CollapsibleContent>
+      </Collapsible>
 
-      <div className="space-y-4 mt-4 flex flex-col-reverse">
-        {stories.length === 0 ? (
-          <p>No stories available</p>
-        ) : (
-          stories.map((story: Story, index) => (
-            <div key={`${story.id}-${index}`} className="border m-5 rounded-md shadow-md">
-              <Input
-                type="text"
-                defaultValue={story.title}
-                className="m-4 w-300"
-                onChange={(e) => setTitle(e.target.value)} // Capture title changes
-              />
-              <MdEditor modelValue={story.text} onChange={setText} preview={false} style={{ height: '350px' }}></MdEditor>
-              {storyMessages[story.id] && (
-                <div
-                  className={`p-4 mb-4 text-sm rounded-lg mt-3 ${storyMessages[story.id].includes("Uspešno")
-                    ? "bg-green-50 text-green-800"
-                    : "bg-red-50 text-red-800"
-                    }`}
-                  role="alert"
-                >
-                  <span>{storyMessages[story.id]}</span>
+      <Card className="w-full max-w-3xl mx-auto mt-4">
+        <CardHeader className='flex flex-row justify-between'>
+          <CardTitle className="text-2xl font-bold mt-2">Uredi Priče</CardTitle>
+          <Button onClick={addNewStory}>Dodaj novu Priču</Button>
+        </CardHeader>
+
+        <CardContent>
+          <div className="space-y-4 mt-4 flex flex-col-reverse">
+            {stories.length === 0 ? (
+              <p>Ne postoje priče</p>
+            ) : (
+              stories.map((story: Story, index) => (
+                <div key={`${story.id}-${index}`} className="border m-5 rounded-md shadow-md">
+                  <DropdownMenuLabel className='p-3'>Naslov Priče</DropdownMenuLabel>
+                  <Input
+                    type="text"
+                    defaultValue={story.title}
+                    className="m-2 w-300"
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <MdEditor
+                    modelValue={story.text}
+                    onChange={setText}
+                    preview={false}
+                    style={{ height: '300px' }}
+                    toolbars={[
+                      'bold',
+                      'underline',
+                      'italic',
+                    ]}
+                  >
+
+                  </MdEditor>
+                  {storyMessages[story.id] && (
+                    <div
+                      className={`p-4 mb-4 text-sm rounded-lg mt-3 ${storyMessages[story.id].includes("Uspešno")
+                        ? "bg-green-50 text-green-800"
+                        : "bg-red-50 text-red-800"
+                        }`}
+                      role="alert"
+                    >
+                      <span>{storyMessages[story.id]}</span>
+                    </div>
+                  )}
+                  <div className='flex justify-evenly'>
+                    <Button onClick={() => deleteStory(story.id)} variant={'destructive'} className='m-4 w-full'>
+                      <Trash2 className="h-5 w-5" />
+                    </Button>
+                    <Button onClick={() => updateStory(story.id)} className='m-4 w-full'>Sačuvaj</Button>
+                  </div>
                 </div>
-              )}
-              <div className='flex justify-evenly'>
-                <Button onClick={() => deleteStory(story.id)} variant={'destructive'} className='m-4'>Obriši Priču</Button>
-                <Button onClick={() => updateStory(story.id)} className='m-4'>Sacuvaj Priču</Button>
-              </div>
-            </div>
-          ))
-        )}
-      </div>
+              ))
+            )}
+          </div>
+        </CardContent>
+      </Card>
+
+
+
     </div >
   );
 }
