596858e24afda6b071b8e1909d0b4abeb9850809
MihajloZivkovic1 <85947846+MihajloZivkovic1@users.noreply.github.com>
2024-12-11 15:21:28 +0100

    da

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -5,5 +5,7 @@ import { NextResponse } from 'next/server';
 export async function POST() {
   (await cookies()).set('session', '', { expires: new Date(0) });
   return NextResponse.redirect('https://plocica-front.vercel.app/login');
+
+
   // return NextResponse.redirect(' http://localhost:3001/login');
 }
